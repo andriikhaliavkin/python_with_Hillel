7436e8ab4c28f9ba8ab01ab939ee79f6dd57acd4
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-10-04 23:37:13 +0200

    Home work 4 task 2

--- home_work_4_Andrii_Khaliavkin.py
+++ home_work_4_Andrii_Khaliavkin.py
@@ -20,8 +20,8 @@ for word in word_list_from_random_sentence:
     list_of_letters_in_the_word = []
     list_of_letters_in_the_word.extend(word)
 
-    for letter in range(len(list_of_letters_in_the_word)-1):
-        if list_of_letters_in_the_word[letter] in vowels and list_of_letters_in_the_word[letter+1] in vowels:
+    for letter in range(len(list_of_letters_in_the_word) - 1):
+        if list_of_letters_in_the_word[letter] in vowels and list_of_letters_in_the_word[letter + 1] in vowels:
             words_with_pair_vowels.add(word)
 
 if len(words_with_pair_vowels) == 0:
@@ -37,3 +37,22 @@ print(
 # { "cito": 47.999, "BB_studio" 42.999, "momo": 49.999, "main-service": 37.245, "buy.now": 38.324, "x-store": 37.166,
 # "the_partner": 38.988, "store": 37.720, "rozetka": 38.003}. Напишіть код, який знайде і виведе на екран назви магазинів,
 # ціни яких попадають в діапазон між мінімальною і максимальною ціною.
+
+stores_with_prices = {"cito": 47.999, "BB_studio": 42.999, "momo": 49.999, "main-service": 37.245, "buy.now": 38.324,
+                      "x-store": 37.166, "the_partner": 38.988, "store": 37.720, "rozetka": 38.003}
+
+while True:
+    try:
+        min_price = float(input('provide minimum price: '))
+        max_price = float(input('provide maximum price: '))
+        if min_price < 0 or max_price < 0 or max_price < min_price:
+            print('Please enter correct price range (without negative values, max price should be higher then min '
+                  'price.)')
+            continue
+        break
+    except ValueError as e:
+        print('Please enter prices in numbers: ')
+
+print(min_price)
+print(max_price)
+
