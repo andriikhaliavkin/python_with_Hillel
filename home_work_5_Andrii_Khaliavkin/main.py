5ae9a30d019ac6f9c0d707bd6794facf5c5b04d3
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-10-08 16:51:12 +0200

    Home work 5

--- home_work_5_Andrii_Khaliavkin/main.py
+++ home_work_5_Andrii_Khaliavkin/main.py
@@ -1,6 +1,9 @@
 import requests
 
+import pandas as pd
+
+
 r = requests.get('http://api.open-notify.org/astros.json')
-print(r.json())
-
-
+people = r.json()['people']
+pd_people = pd.DataFrame(people)
+print(pd_people)
