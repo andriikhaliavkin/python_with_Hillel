0bf9117700ee3473d60150e067ed782ad2c2d613
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-10-08 18:17:18 +0200

    Home work 5

--- home_work_5_Andrii_Khaliavkin/main.py
+++ home_work_5_Andrii_Khaliavkin/main.py
@@ -2,8 +2,30 @@ import requests
 
 import pandas as pd
 
+print(
+    '''---------------------------------TASK 1----------------------------------------''')
+# завдання 1
+# урл http://api.open-notify.org/astros.json
+# вивести список всіх астронавтів, що перебувають в даний момент на орбіті (дані не фейкові, оновлюються в режимі реального часу)
 
 r = requests.get('http://api.open-notify.org/astros.json')
 people = r.json()['people']
 pd_people = pd.DataFrame(people)
 print(pd_people)
+
+print(
+    '''---------------------------------TASK 2----------------------------------------''')
+# завдання 2
+# апі погоди (всі токени я для вас вже прописав)
+# https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=47503e85fabbabc93cff28c52398ae97&units=metric
+# де city_name - назва міста на аглійській мові (наприклад, odesa, kyiv, lviv, london)
+# погода змінюється, як і місто. яке ви введете
+# роздрукувати тепрературу та швидкість вітру. з вказівкою міста, яке було вибране
+
+api_key = '47503e85fabbabc93cff28c52398ae97'
+city_name = input('Enter city name: ')
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'
+r = requests.get(url)
+weather = r.json()
+print(f'Temperature in {city_name} is {weather["main"]["temp"]} C')
+print(f'Wind speed in {city_name} is {weather["wind"]["speed"]} m/s')
