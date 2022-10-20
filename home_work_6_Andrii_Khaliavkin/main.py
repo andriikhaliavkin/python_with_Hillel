a245fb9a82cc6dcc15aa14693d9989db779016b8
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-10-20 13:43:43 +0200

    Home work 6 task 2 - solution

--- home_work_6_Andrii_Khaliavkin/main.py
+++ home_work_6_Andrii_Khaliavkin/main.py
@@ -13,4 +13,25 @@ def convert_to_float(arg):
         return 0
 
 
-convert_to_float(1)
+print(convert_to_float('123'))
+
+print(
+    '''---------------------------------TASK 2----------------------------------------''')
+
+
+# Напишіть функцію, що приймає два аргументи. Функція повинна
+# якщо аргументи відносяться до числових типів (int, float) - повернути перемножене значення цих аргументів,
+# якщо обидва аргументи це строки (str) - обʼєднати в одну строку та повернути
+# у будь-якому іншому випадку повернути кортеж з цих аргументів
+
+def multiply_or_concat(arg1, arg2):
+    """Take two args, return multiplication or concatenation"""
+    if isinstance(arg1, (int, float)) and isinstance(arg2, (int, float)):
+        return arg1 * arg2
+    elif isinstance(arg1, str) and isinstance(arg2, str):
+        return arg1 + arg2
+    else:
+        return arg1, arg2
+
+
+print(multiply_or_concat('d', 'f'))
