2d9c05f8e115357809c2ca4f452edef9940e4d48
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-10-28 21:33:09 +0200

    Home work 10 init

--- home_work_10_Andrii_Khaliavkin/library.py
+++ home_work_10_Andrii_Khaliavkin/library.py
@@ -1,1 +1,48 @@
 
+# потрібно дописати is_string_capitalized (в тому числі докстрінги) - очікування - прохід тестів
+# assert is_string_capitalized('My name is David') is False
+# assert is_string_capitalized('I love playing') is True
+# assert is_string_capitalized('') is True
+# assert is_string_capitalized('565656') is True
+
+def is_string_capitalized(string):
+    """
+    This function checks if the first letter of the string is capitalized.
+    :param string: string
+    :return: bool
+    """
+    if string == '':
+        return True
+    elif string[0].isupper():
+        return True
+    elif string[0].isalnum():
+        return True
+    else:
+        return False
+
+
+assert is_string_capitalized('My name is David') is True
+assert is_string_capitalized('I love playing') is True
+assert is_string_capitalized('') is True
+assert is_string_capitalized('565656') is True
+
+
+# write a function to check if the given number is odd or even
+
+def is_odd_or_even(number):
+    """
+    This function checks if the given number is odd or even.
+    :param number: int
+    :return: str
+    """
+    if number % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+
+assert is_odd_or_even(5) == 'odd'
+assert is_odd_or_even(6) == 'even'
+assert is_odd_or_even(0) == 'even'
+assert is_odd_or_even(-1) == 'odd'
+
